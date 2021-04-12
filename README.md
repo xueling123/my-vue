f6daa3c179c9749714da18bb4c3f96c1771324df
lingwei <lingwei@asiainfo.com>
2021-04-12 10:09:43 +0800

    提交代码

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# my-app
+# my-vue
 
 ## Project setup
 ```
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,10 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<style lang="scss">
+  .about{
+    h1{
+      color: red;
+    }
+  }
+</style>
